3f60b76df24cdd52d29cfee89af063444d44f6e5
nsc-ko-kurita <63280543+nsc-ko-kurita@users.noreply.github.com>
2020-04-13 12:33:11 +0900

    Add files via upload

---
+++ human.js
@@ -0,0 +1,12 @@
+class RPG {
+    constructor(name, HP, MP, buki, kougeki){
+        this.name = name;
+        this.HP = HP;
+        this.MP = MP;
+        this.buki = buki;
+        this.kougeki = kougeki;
+    }
+     
+}
+
+
